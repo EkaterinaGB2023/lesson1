20e491b339e913231b755f71d647b17d7a0fc322
Ekaterina <katushka26@yandex.ru>
2023-02-09 15:45:33 +0300

    Dog and friends - version 1

---
+++ Project8/Program.cs
@@ -0,0 +1,24 @@
+﻿int dist = 10000; //distance
+int ffs = 1; // first friend speed
+int sfs = 2; //second friend speed
+int ds = 5; // dog speed
+int fr = 2; // direction of the dog from the first to the second
+int T = 0; 
+int count = 0;
+
+while(dist < 10)
+    if(fr == 1)
+    {
+    T = dist / (ffs + ds);
+    fr = 2;
+    }
+    else
+    {
+    T = dist / (sfs + ds);
+    fr = 1;
+    }
+dist = dist - (ffs + sfs) * T;
+count++;
+
+Console.Write("Собака пробежала раз");
+Console.WriteLine(count);
